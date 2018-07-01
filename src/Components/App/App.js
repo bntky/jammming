eeecd6d77337e5400d5ffd9556821a2bda5f26c2
Brendan Kirby <bkirby@bntky.com>
2018-06-30 20:10:29 -0700

    Fix bugs to get it working with fixed data

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchResults: [
+                {
+                    id: 1,
+                    name: 'Mr. Klaw',
+                    album: 'Miscellaneous T The B Side Remix Compilation',
+                    artist: 'They Might Be Giants'
+                },
+                {
+                    id: 2,
+                    name: 'Birds Fly',
+                    album: 'Miscellaneous T The B Side Remix Compilation',
+                    artist: 'They Might Be Giants'
+                },
+                {
+                    id: 3,
+                    name: 'Nightgown of the Sullen Moon',
+                    album: 'Miscellaneous T The B Side Remix Compilation',
+                    artist: 'They Might Be Giants'
+                }
+            ],
+            playlistName: 'empty',
+            playlistTracks: []
+        };
+    }
+
     render() {
         return (
             <div>
@@ -13,8 +40,9 @@ class App extends Component {
               <div className="App">
                 <SearchBar />
                 <div className="App-playlist">
-                  <SearchResults />
-                  <Playlist />
+                  <SearchResults searchResults={ this.state.searchResults } />
+                  <Playlist playlistName={ this.state.playlistName }
+                            playlistTracks={ this.state.playlistTracks } />
                 </div>
               </div>
             </div>
