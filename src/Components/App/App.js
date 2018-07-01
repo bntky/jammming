b3e042c3ed010134fd009d69a2542a70c93ec477
Brendan Kirby <bkirby@bntky.com>
2018-07-01 11:48:44 -0700

    Make changing the playlist name work

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -33,6 +33,7 @@ class App extends Component {
         };
         this.addTrack = this.addTrack.bind(this);
         this.removeTrack = this.removeTrack.bind(this);
+        this.updatePlaylistName = this.updatePlaylistName.bind(this);
     }
 
     addTrack(track) {
@@ -52,6 +53,10 @@ class App extends Component {
         this.setState({ playlistTracks: curTracks });
     }
 
+    updatePlaylistName(name) {
+        this.setState({ playlistName: name });
+    }
+
     render() {
         return (
             <div>
@@ -63,7 +68,8 @@ class App extends Component {
                                  onAdd={ this.addTrack } />
                   <Playlist playlistName={ this.state.playlistName }
                             playlistTracks={ this.state.playlistTracks }
-                            onRemove={ this.removeTrack } />
+                            onRemove={ this.removeTrack }
+                            onNameChange={ this.updatePlaylistName } />
                 </div>
               </div>
             </div>
